2cb514019234b2602f450828e801527e1ad1f780
Nguyễn Minh Quân <165236869+MihQuannn7799@users.noreply.github.com>
2025-05-18 22:59:09 +0700

    Update ddl_goal.sql

--- scripts/gold/ddl_goal.sql
+++ scripts/gold/ddl_goal.sql
@@ -15,18 +15,18 @@ IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
     DROP VIEW gold.dim_customers;
 CREATE VIEW gold.dim_customer AS
 SELECT 
-ROW_NUMBER()  OVER (ORDER BY cst_id) AS custermer_key,
-	ci.cst_id			      AS customer_id,
-	ci.cst_key			    AS customer_number,
-	ci.cst_firstname	  AS firstname,
-	ci.cst_lastname		  AS lastname,
+ROW_NUMBER()  OVER (ORDER BY cst_id)    AS custermer_key,
+	ci.cst_id			AS customer_id,
+	ci.cst_key			AS customer_number,
+	ci.cst_firstname	 	AS firstname,
+	ci.cst_lastname		 	AS lastname,
 	la.cntry		        AS country,
-	ci.cst_marital_status AS marital_status,
+	ci.cst_marital_status 		AS marital_status,
 	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr --CRM is the master for gender
 		 ELSE COALESCE(ca.gen,'n/a')
-	END					        AS gender,
+	END 				AS gender,
 	ca.bdate		        AS birthday,
-	ci.cst_create_date  AS create_date
+	ci.cst_create_date  		AS create_date
 		
 FROM silver.crm_cust_info ci
 LEFT JOIN silver.erp_cust_a101 ca
@@ -36,21 +36,21 @@ ON		  ci.cst_key = la.cid
 
   
 -- Create Dimension: gold.dim_products
-  IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
+IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
     DROP VIEW gold.dim_products;
 CREATE VIEW gold.dim_products AS 
 SELECT 
 ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
-pn.prd_id		    AS product_id,	
-pn.prd_key	  	AS product_number,
-pn.prd_nm		    AS product_name,
-pn.cat_id		    As category_id,
-pc.cat			    AS category,
-pc.subcat		    AS subcategory,
-pc.maintenance,
-pn.prd_cost	  	AS cost,
-pn.prd_line		  AS product_line,
-pn.prd_start_dt AS start_date
+	pn.prd_id		    AS product_id,	
+	pn.prd_key	  	    AS product_number,
+	pn.prd_nm		    AS product_name,
+	pn.cat_id		    As category_id,
+	pc.cat			    AS category,
+	pc.subcat		    AS subcategory,
+	pc.maintenance,
+	pn.prd_cost	  	    AS cost,
+	pn.prd_line		    AS product_line,
+	pn.prd_start_dt 	    AS start_date
 FROM silver.crm_prd_info pn
 LEFT JOIN silver.erp_px_cat_g1v2 pc
 ON pn.cat_id = pc.id
@@ -58,11 +58,11 @@ WHERE prd_end_dt IS NULL
   
 
 -- Create Fact Table: gold.fact_sales
-  IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
+IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
     DROP VIEW gold.fact_sales;
 CREATE VIEW gold.fact_sales AS
 SELECT 
-    sd.sls_ord_num      AS order_number,
+    sd.sls_ord_num            AS order_number,
 	pr.product_key,
 	cu.custermer_key,
 	sd.sls_order_dt       AS order_date,
